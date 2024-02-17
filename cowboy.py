cfe39b7e7d7c495fc64e31875f1d9cd0d0f3ae17
Morning Dove Nelson <morningdhansen@gmail.com>
2024-02-17 14:06:14 -0700

    correct how decisions are called in cowboy.py

--- cowboy.py
+++ cowboy.py
@@ -1,13 +1,13 @@
-from fractions import Fraction
 from interface import Interface
 import cowboy_decisions
 
 
 class Cowboy(Interface):
 
-
     def story(self):
-        choices = ["Do you steal his clothes to blend in with everyone else? ", "Do you jump out from behind the counter and yell DON\'T SHOOT!!!"]
+        print()
+        print(cowboy_decisions.decisions["decision1"])
+        choices = ["Blend in with everyone else? ", "Jump out from behind the counter?"]
         print()
         choice = self.display_options(choices)
         if choice == "1":
@@ -18,17 +18,6 @@ class Cowboy(Interface):
             self.wrong_choice()
 
 
-    def wrong_choice(self):
-        self.remote_battery -= 3
-        print(f"""You have made the wrong choice and ended up in a precarious situation. 
-Your remote is down to {Fraction(self.remote_battery/8)}.
-                """)
 
-
-def main():
-    cowboy = Cowboy()
-    print(cowboy_decisions.decisions["decision1"])
-    cowboy.story()
-
-if __name__ == "__main__":
-    main()
+cowboy = Cowboy()
+cowboy.story()
--- cowboy_decisions.py
+++ cowboy_decisions.py
@@ -1,5 +1,10 @@
 decisions = {
-    "decision1": """You quickly find yourself in another world. You hear shots being fired left and right.
+
+    "decision1" : """You quickly find yourself in another world. You hear shots being fired left and right.
 You hear bullets whizzing by your head. You duck behind the counter, hoping no one noticed you popping up out of no where.
-You see a man passed out behind the counter presumably from drinking."""
+You see a man passed out behind the counter presumably from drinking.
+
+Do you steal his clothes to blend in with everyone else? or Do you jump out from behind the counter and yell DON'T SHOOT!!!""",
+
+
 }
