b0a79c6ec13e5949955a2b7ec6ddc8c9be11b961
Morning Dove Nelson <morningdhansen@gmail.com>
2024-02-17 16:31:18 -0700

    fix bug with how classes are called

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from interface import Interface
 from future import Vault
+from desolate import Desolate
+from cowboy import Cowboy
 
 
 class Main(Interface):
+
+    def __init__(self):
+        self.vault = Vault()
+        self.desolate = Desolate()
+        self.cowboy = Cowboy()
 
     def map():
         print("You project a holographic \
@@ -12,22 +19,21 @@ class Main(Interface):
     of the maze of vents you need to go \
     Left, Right, Right, Left, Straight")
         
-def main():
-    print("""You project a holographic
-map of the vent sytem of the 
-compound surrounding the vault""")
-    print("""From the beginning 
-of the maze of vents you need to go
-Left, Right, Right, Left, Straight""")
-    vault = Vault()
-    vault.navigate_vents()
-
-    #Desolate.arrived()
-    #Cowboy.story() 
+    def run(self):
+        print("""You project a holographic
+    map of the vent sytem of the 
+    compound surrounding the vault""")
+        print("""From the beginning 
+    of the maze of vents you need to go
+    Left, Right, Right, Left, Straight""")
+        self.vault.navigate_vents()
+        self.desolate.arrived()
+        self.cowboy.story()
     #Add Wizard.py
        
    
 
 
 if __name__ == "__main__":
-    main()
+    main = Main()
+    main.run()
