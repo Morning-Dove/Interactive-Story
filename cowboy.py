f212016cc8c08b213173c815276d75731598ccdd
Morning Dove Nelson <morningdhansen@gmail.com>
2024-02-18 16:48:39 -0700

    Half of the story line complete

--- cowboy.py
+++ cowboy.py
@@ -1,5 +1,7 @@
+import sys
 from interface import Interface
 import cowboy_decisions
+
 
 
 class Cowboy(Interface):
@@ -11,13 +13,84 @@ class Cowboy(Interface):
         print()
         choice = self.display_options(choices)
         if choice == "1":
-            pass
+            print(cowboy_decisions.decisions["decision2"])
+            self.blend_in()
         elif choice  == "2":
+            print(cowboy_decisions.decisions["decision3"])
             pass
         else:
             self.wrong_choice()
 
 
+    def blend_in(self):
+        print()
+        print(cowboy_decisions.decisions["decision4"])
+        choices = ["Stop and get them a drink?", "Keep walking out?"]
+        print()
+        choice = self.display_options(choices)
+        if choice == "1":
+            print(cowboy_decisions.decisions["decision5"])
+            self.become_the_bartender()
+        elif choice  == "2":
+            print(cowboy_decisions.decisions["decision6"])
+            self.the_end()
+        else:
+            self.wrong_choice()
+
+
+    def become_the_bartender(self):
+        print()
+        print(cowboy_decisions.decisions["decision7"])
+        choices = ["Befriend the Saloon patrons?", "Continue to sneak away?"]
+        print()
+        choice = self.display_options(choices)
+        if choice == "1":
+            print(cowboy_decisions.decisions["decision8"])
+            self.break_in()
+        elif choice  == "2":
+            print(cowboy_decisions.decisions["decision9"])
+            self.the_end()
+        else:
+            self.wrong_choice()
+
+    def break_in(self):
+        print()
+        print(cowboy_decisions.decisions["decision10"])
+        choices = ["Convince your new friends?", "Break in alone?"]
+        print()
+        choice = self.display_options(choices)
+        if choice == "1":
+            print(cowboy_decisions.decisions["decision11"])
+            self.break_in()
+        elif choice  == "2":
+            print(cowboy_decisions.decisions["decision12"])
+            self.the_end()
+        else:
+            self.wrong_choice()
+ 
+
+    def do_not_shoot(self):
+        print()
+        print(cowboy_decisions.decisions["decision"])
+        choices = [""]
+        print()
+        choice = self.display_options(choices)
+        if choice == "1":
+            print(cowboy_decisions.decisions["decision"])
+            pass
+        elif choice  == "2":
+            print(cowboy_decisions.decisions["decision"])
+            pass
+        else:
+            self.wrong_choice() 
+
+
+    def the_end(self):
+        print("You don't make it to the charging port. You never make it out of this world.")
+        sys.exit(0)
 
 cowboy = Cowboy()
 cowboy.story()
+
+
+
--- cowboy_decisions.py
+++ cowboy_decisions.py
@@ -1,10 +1,76 @@
 decisions = {
 
+    #Goes to decision2 & decision3
     "decision1" : """You quickly find yourself in another world. You hear shots being fired left and right.
 You hear bullets whizzing by your head. You duck behind the counter, hoping no one noticed you popping up out of no where.
 You see a man passed out behind the counter presumably from drinking.
 
-Do you steal his clothes to blend in with everyone else? or Do you jump out from behind the counter and yell DON'T SHOOT!!!""",
+Do you steal his clothes to blend in with everyone else? 
+                            or 
+Do you jump out from behind the counter and yell DON'T SHOOT!!!""",
+
+    #Goes to decision4
+    "decision2" : """You stole this random person's clothes and left them with your clothes that would stand out and cause someone to ask questions. 
+You need to quietly and quickly find the charging port. You haven't seen guns in years. They make you a little nervous. 
+You decide you do not want to stay here long. You check the map to find out how far a charging port is.""",
+
+    "decision3" : """Do you jump out from behind the counter and yell DON'T SHOOT!!!""",
+    
+    #Goes to decision5 & decision6
+    "decision4" : """You are in luck there is a charging port not far from here. It is only about a days walk from here.
+You begin to casually walk out from behind the bar. When someone tells you to stop and get them a drink. 
+You realize they think you are the bartender.
+
+Do you stop and get them a drink?
+            or
+Do you tell them you just quit your job and keep walking out?
+                """,
+
+    #Goes to decision7
+    "decision5" : """You stop and get them a drink. They ask about you saying how they have never seen you in town before. 
+You tell them about how you are just passing through and you wouldn't be in town for long.
+                """,
+
+    #the_end()
+    "decision6" : """Tell them you just quit your job. They start yelling and getting upset at you for trying to walk out. 
+You don't make it very far out of the Saloon before someone stops to ask who you are.
+They mention how they have never seen you before and hadn't heard about anyone new coming into town.
+They begin questioning the first story you told them. They are afraid you are trying to come and do harm within their town. 
+You try to explain to them that is not what is happening, but it is to late. They pull a gun on you... bang. bang. 
+                    """,
+
+    #Goes to decision8 & decision9
+    "decision7" : "Do you befriend the saloon patrons? or Sneak out the back when no one is looking?",
+
+    #Goes to decision10
+    "decision8" : """You stick around behind the bar for awhile serving drinks to everyone. 
+You end up befriending the saloon patrons. You are able to go home for the night with one of your new friends.
+This gives you the chance to take a shower and sleep in a real bed.""",
+
+    #the_end()
+    "decision9" : """You sneak away from the saloon patrons. You look at the map to the charging port and realize it is in building about a block away.
+You walk down to the building. The building doesn't have any clear markings outside so you go in. Only to realize it is a bank. The map shows you it is in the bank vault.
+You quickly come up with a plan that does not work and lands you in prison for life.
+                    """,
+
+    #Goes to decision11 & decision12
+    "decision10" : """The next morning you wake up and check your map to see how far you are from the charging port. 
+You realize it is only a few buildings down from the saloon. You decide to take a walk by the building to see how you can get your remote charged and make it out of here.
+After walking by you realize it is a bank and the charging port is in the vault. You continue walking to go back down to the saloon.
+
+Do you convince your new friends to help you break into the vault?
+                            or
+Do you figure out how to do it alone?
+                    """,
+
+    "decision11" : """You have made it back to the saloon and begin to tell your new friends how there is something in a lock box in the bank that you need.
+You begin to convince them to help you come up with a plan to break into the bank. As long as they get something out of it too they will help. A plan has been devised.
+You will be breaking into th bank vaul that night.
+                    """,
+
+    "decision12" : """You are still nervous about getting to close to anyone in this cowboy town you end up deciding to break into the bank by yourself. 
+You make your plan to break in that night. You try to 
+                    """,
 
 
 }
