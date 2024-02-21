1e15fd879fc82d88068da9022cb6b15340b82e43
Morning Dove Nelson <morningdhansen@gmail.com>
2024-02-21 08:25:43 -0700

    Run program starting with future.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from interface import Interface
-#from future import Vault
+from future import Vault
 from desolate import Desolate
 from cowboy import Cowboy
 #from wizard import
@@ -8,13 +8,13 @@ from cowboy import Cowboy
 class Main(Interface):
 
     def __init__(self):
-        #self.vault = Vault()
+        self.vault = Vault()
         self.desolate = Desolate()
         self.cowboy = Cowboy()
         #Add wizard
                
     def run(self):
-        #self.vault.navigate_vents()      
+        self.vault.navigate_vents()      
         self.desolate.arrived()
         self.cowboy.story()
         #Add Wizard.py
